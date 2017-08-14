e3242c514a3599f29775f8fa7fcec1283b550300
Ronalson Filho <contact@ronalson.com.br>
2017-08-14 19:45:20 -0300

    db:migrate with right variables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170814210743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "instruments", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.date     "checkout"
+    t.date     "checkin"
+    t.boolean  "blocked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
