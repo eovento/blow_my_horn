5cd85667c425a694879ffb0b0391f9a2b44ef537
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-15 19:25:26 -0300

    added fields to Users: bio, avatar, phone, address, document

---
+++ db/migrate/20170815221151_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :bio, :text
+    add_column :users, :phone, :string
+    add_column :users, :address, :string
+    add_column :users, :document, :string
+    add_column :users, :avatar, :string
+  end
+end
