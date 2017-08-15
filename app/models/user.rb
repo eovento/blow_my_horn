5da09e7c418b515365ba2de36695463d5a6f016b
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-15 12:26:05 -0300

    added references to model -#14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :instruments
+  has_many :rentals
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
