1587836d03257d3067c4986489087f5d497cc7cf
Luiz Gustavo Neiva <luizgzn@gmail.com>
2017-08-16 16:16:08 -0300

    updating application.js
    require tree need to be the last command so it can require other js files in the folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,6 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
-//= require_tree .
 //= require moment
 //= require bootstrap-datetimepicker
+//= require_tree .
