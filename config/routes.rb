55341be6da7c23965e33959f2bb63a7c7bc6dc40
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-16 15:35:09 -0300

    rental partial form working on instrument show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
+
+  post "instruments/:id/rent", to: "rentals#create", as: "rent_instrument"
   resources :instruments
   resources :rentals
   devise_for :users,
