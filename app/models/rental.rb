62f936108fd397cbe6fd118623a03b708f4d6fcf
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-17 19:39:01 -0300

    Flashes for error messages and succesfull rental, no confirmation page - closes #60

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,7 +4,12 @@ class Rental < ApplicationRecord
 
   validate :validate_date_in_present, :validate_date_smaller_than_end_date
   validates :user_id, :instrument_id, :start_date, :end_date, presence: true
-  validates :instrument_id, uniqueness: { scope: :start_date}
+  validates :start_date, uniqueness: { scope: :instrument, message: "Instrument already taken!" }
+
+     # Return a scope for all rental overlapping the given rental, including the given rental itself
+  #named_scope :overlapping, lambda { |rental| {
+  #  :conditions => ["id <> ? AND (DATEDIFF(start_date, ?) * DATEDIFF(?, end_date)) >= 0", rental.id, rental.end_date, rental.start_date]
+  #}}
 
   def validate_date_in_present
     if start_date.present? && start_date < Date.today
@@ -14,7 +19,7 @@ class Rental < ApplicationRecord
 
   def validate_date_smaller_than_end_date
     if start_date.present? && start_date > end_date
-      errors.add(:start_date, "Start date needs to be before end date!")
+      errors.add(:end_date, "Start date needs to be before end date!")
     end
   end
 
